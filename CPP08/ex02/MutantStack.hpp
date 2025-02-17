5eabe904cbb919903e1252ceeccff1074b875bc5
Ulas Berke Yildiz <ulyildiz@k2m06s02.42kocaeli.com.tr>
2025-02-17 19:04:28 +0300

    .

--- CPP08/ex02/MutantStack.hpp
+++ CPP08/ex02/MutantStack.hpp
@@ -2,14 +2,13 @@
 # define MUTANTSTACK_HPP
 
 # include <stack>
+# include <vector>
 
-template <typename T>
-class MutantStack: public std::stack<T> {
-	private:
-
+template <typename T, typename C = std::vector<T> >
+class MutantStack: public std::stack<T, C> {
 	public:
-		MutantStack(): std::stack<T, std::vector<int> >() {}
-		MutantStack(const MutantStack &copied): std::stack<T>(copied) {}
+		MutantStack(): std::stack<T, C>() {}
+		MutantStack(const MutantStack &copied): std::stack<T, C>(copied) {}
 		~MutantStack() { this->c.clear(); }
 		MutantStack &operator=(const MutantStack &other)
 		{
@@ -19,23 +18,22 @@ class MutantStack: public std::stack<T> {
 			return *this;
 		}
 
-		typedef typename MutantStack<T>::container_type::iterator iterator;
+		typedef typename MutantStack<T, C>::container_type::iterator iterator;
 		iterator begin() { return this->c.begin(); }
 		iterator end() { return this->c.end(); }
 
-		typedef typename MutantStack<T>::container_type::const_iterator const_iterator;
+		typedef typename MutantStack<T, C>::container_type::const_iterator const_iterator;
 		const_iterator begin() const { return this->c.begin(); }
 		const_iterator end() const { return this->c.end(); }
 
-		typedef typename MutantStack<T>::container_type::reverse_iterator reverse_iterator;
+		typedef typename MutantStack<T, C>::container_type::reverse_iterator reverse_iterator;
 		reverse_iterator rbegin() { return this->c.rbegin(); }
 		reverse_iterator rend() { return this->c.rend(); }
 
-		typedef typename MutantStack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+		typedef typename MutantStack<T, C>::container_type::const_reverse_iterator const_reverse_iterator;
 		const_reverse_iterator rbegin() const { return this->c.rbegin(); }
 		const_reverse_iterator rend() const { return this->c.rend(); }
 
-		
 };
 
 #endif
--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -3,5 +3,45 @@
 
 int	main(void)
 {
+	MutantStack<int> mstack;
+
+	mstack.push(5);
+	mstack.push(17);
+	mstack.push(3);
+	mstack.push(-2);
+	mstack.push(737);
+
+	std::cout << "mstack.top() = " << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << "mstack.size() = " << mstack.size() << std::endl;
+
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+
+	++it;
+	--it;
+
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		std::cout << it.base() << std::endl;
+		++it;
+	}
+
+	std::vector<int> vec;
+
+	for (int i = 0; i < 10; i++)
+		vec.push_back(i);
+
+	std::vector<int>::iterator it2 = vec.begin();
+	std::vector<int>::iterator ite2 = vec.end();
+
+	while (it2 != ite2)
+	{
+		std::cout << *it2 << std::endl;
+		std::cout << it2.base() << std::endl;
+		++it2;
+	}
+
 	return (0);
 }
