221681a7e0a02795b989a0b982aeb65441cb5d38
Ulas Berke Yildiz <ulyildiz@k2m06s02.42kocaeli.com.tr>
2025-02-17 18:40:48 +0300

    .

--- Test/typename.cpp
+++ Test/typename.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <vector>
 class awesome
 {
 public:
@@ -9,17 +9,31 @@ public:
 	int getN() const { return _n; }
 private:
 	int _n;
+
+};
+template<typename A>
+class deneme
+{
+	A a;
+
+public:
+	typedef A::size_type val_type;
+
 };
 
 int main()
 {
+	std::vector<int> v;
+	std::vector<std::string>::size_type i = 42;
 
 	std::string str = "Hello";
 
 	std::cout << str << std::endl;
 
-	typename awesome::a s = str.size();
+	typename std::string::size_type s = str.size();
 	std::cout << s << std::endl;
 	
+	typedef typename std::string::size_type n;
+	n x = 42;
 	return 0;
 }
