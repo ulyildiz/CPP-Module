7553bbeada051d5b8a27d127505fe6022cdccd79
ulyildiz <ulashjob@gmail.com>
2025-06-24 18:53:08 +0300

    .

--- CPP09/ex02/PmergeMe.hpp
+++ CPP09/ex02/PmergeMe.hpp
@@ -6,6 +6,8 @@
 # include <sstream>
 # include <typeinfo>
 # include <cstdlib>
+# include <deque>
+# include <vector>
 
 class PmergeMe
 {
@@ -30,68 +32,24 @@ class PmergeMe
 				std::cout << *it << " ";
 			std::cout << std::endl;
 		}
-		
-		template <typename T>
-		static void	parseInput(T &container, char *argv)
-		{
-			std::string	str(argv);
-			std::stringstream	ss(str);
-		
-			while (ss >> str)
-			{
-				if (str.find_first_not_of("0123456789") != std::string::npos)
-				{
-					std::cerr << "Invalid input" << std::endl;
-					throw std::invalid_argument("Invalid input");
-				}
-				container.push_back(std::atoi(str.c_str()));
-			}
-		}
-		
-		template <typename T>
-		void	fordJohnson(T &main, T &pend)
+
+		void	fordJohnson(std::deque<int> &container) throw()
 		{
 			_startTimer();
-			makePair(main, pend);
-
+			// Implement the Ford-Johnson algorithm here
 			_endTimer();
-			
-			std::cout << "Time to process a range of " << main.size() << " elements with std::" << typeid(main).name();
-			_printDuration(); 
+			std::cout << "Ford-Johnson algorithm executed";
+			_printDuration();
 		}
-		
-		template <typename T>
-		void	makePair(T &main, T &pend)
+
+		void	fordJohnson(std::vector<int> &container) throw()
 		{
-			if (main.size() <= 1)
-				return ;
-		
-			bool	isOdd = false;
-			typename T::iterator	it = main.begin();
-
-			while (it != main.end())
-			{
-				if (isOdd)
-				{
-					pend.push_back(*it);
-					it = main.erase(it);
-				}
-				else
-				{
-					++it;
-				}
-				isOdd = !isOdd;
-			}
+			_startTimer();
+			// Implement the Ford-Johnson algorithm here
+			_endTimer();
+			std::cout << "Ford-Johnson algorithm executed";
+			_printDuration();
 		}
-
-		template <typename T>
-		void	mergeInsertionSort(T &main, T &pend, int &start, int &end)
-		{
-
-		}
-		template <typename T>
-		void	binaryInsertion(T &container, int low, int high, int key);
-		
 };
 
 #endif
--- CPP09/ex02/main.cpp
+++ CPP09/ex02/main.cpp
@@ -1,6 +1,5 @@
 #include "PmergeMe.hpp"
-#include <deque>
-#include <vector>
+
 
 int main(int argc, char *argv[])
 {
@@ -10,29 +9,6 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	std::vector<int>	v_main;
-	std::vector<int>	v_pend;
-	std::deque<int>		d_main;
-	std::deque<int>		d_pend;
-
-	try {
-		PmergeMe::parseInput<std::vector<int> >(v_main, argv[1]);
-		PmergeMe::parseInput<std::deque<int> >(d_main, argv[1]);
-	} catch (std::exception &e) {
-		std::cerr << e.what() << std::endl;
-		return (1);
-	}
-
-	PmergeMe pm;
-
-	std::cout << "Before:" ;
-	pm.printContainer<std::vector<int> >(v_main);
-
-	pm.fordJohnson<std::vector<int> >(v_main, v_pend);
-	pm.fordJohnson<std::deque<int> >(d_main, d_pend);
-
-	std::cout << "After:" ;
-	pm.printContainer<std::deque<int> >(d_main);
-
+	
 	return (0);
 }
